d1cd4dc9edb64f6ffdd4851718674e490caf7582
Maria Frih <maria.frih@gmail.com>
2023-12-06 21:05:22 +0100

    add : iteration 3.1  and 3.2 done

--- index.js
+++ index.js
@@ -23,3 +23,20 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+
+// 3.1 : Uppercase separated by space
+
+const upperCasedHacker1 = hacker1.toUpperCase();
+
+// reminder .split() turns the string into an array, each index of the array is a letter.
+// join() is defined by MDN as
+// The join() method of Array instances creates and returns a new string by concatenating all of the elements in this array,
+// separated by commas or a specified separator string.
+
+const separatedHacker1Name = upperCasedHacker1.split("").join(" ");
+console.log(separatedHacker1Name);
+
+// 3.2 : backwards (reverse ;-))
+
+const reversedName = hacker1.split("").reverse().join("");
+console.log(reversedName);
